84fb204709fcadf7f397e256bd42d899209aa423
hugginssd <hugginsmafigu@gmail.com>
2019-04-28 20:18:48 +0200

    Added data access layer

--- Realtime Stats Metrics And Analysis/BLL/CategoriesBLL.cs
+++ Realtime Stats Metrics And Analysis/BLL/CategoriesBLL.cs
@@ -8,5 +8,17 @@ namespace Realtime_Stats_Metrics_And_Analysis.BLL
 {
     class CategoriesBLL
     {
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+
+        public DateTime AddedDate { get; set; }
+
+        public int AddedBy { get; set; }
+
     }
 }
--- Realtime Stats Metrics And Analysis/BLL/DealerCustomerBLL.cs
+++ Realtime Stats Metrics And Analysis/BLL/DealerCustomerBLL.cs
@@ -8,5 +8,20 @@ namespace Realtime_Stats_Metrics_And_Analysis.BLL
 {
     class DealerCustomerBLL
     {
+        public int ID { get; set; }
+
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Contact { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        public int AddedBy { get; set; }
     }
 }
